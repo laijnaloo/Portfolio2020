71e6de94d097537b64eb0a583703ba24b4e7a2d0
laijnaloo <lina.agneta.andersson@gmail.com>
2020-02-15 21:53:32 +0100

    Added assets

---
+++ docs/precache-manifest.44db2e6ebfa62039e2c148a8a47a0e99.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96047f84a3817618036169009126c63a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57bdd5fa0d76c2719ea3",
+    "url": "/static/css/main.d12bafae.chunk.css"
+  },
+  {
+    "revision": "ca905df25f7f68dd185d",
+    "url": "/static/js/2.5152a6dd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5152a6dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57bdd5fa0d76c2719ea3",
+    "url": "/static/js/main.4908cc7c.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "05ee28b80bd6288d43ba4fd4940443fd",
+    "url": "/static/media/Einride.05ee28b8.jpg"
+  },
+  {
+    "revision": "120d005809f07c9ae8a9d233eb5647d2",
+    "url": "/static/media/Gelasio-Medium.120d0058.ttf"
+  },
+  {
+    "revision": "90a4d9ca77ab52c1dcec01808cb69cf4",
+    "url": "/static/media/Plants.90a4d9ca.png"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "98ea4655bd8ab35e06d421508207ccdc",
+    "url": "/static/media/bron.98ea4655.jpg"
+  },
+  {
+    "revision": "afc51a0542541b196a88985eda9d41f4",
+    "url": "/static/media/nn.afc51a05.png"
+  }
+]);
